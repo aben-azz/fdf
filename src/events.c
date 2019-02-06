7aa7a2a8ad8e8c5299880b499a130148c15e3dda
Achraf <macadelic1030@gmail.com>
2019-02-06 14:30:07 +0100

    ui

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 08:58:10 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/02/06 13:39:45 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/02/06 14:29:55 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ t_key g_key[] = {
 
 int		evt_live_mouse_move(int x, int y, t_mlx *m)
 {
+	mlx_clear_window(m->i, m->w);
 	g_mouse = (t_xy){.x = x, .y = y};
-	clear(*m);
 	if (SHOW_LIVE_MOUSE)
 	{
 		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
@@ -48,7 +48,6 @@ int		evt_live_key_clicked(int key, t_mlx *m)
 	while (g_key[++i].key)
 		if (g_key[i].key == key)
 			g_key[i].function && g_key[i].function(*m);
-	clear(*m);
 	if (SHOW_LIVE_KEY)
 		string(*m, (t_xy){.x = g_mouse.x + 40, .y = g_mouse.y}, 0x00FF00,
 			ft_itoa_base(key, 10, 0));
@@ -61,7 +60,7 @@ int		evt_live_key_pressed(int key, t_mlx *m)
 	int i;
 
 	i = -1;
-	clear(*m);
+
 
 	while (g_key[++i].key)
 		if (g_key[i].key == key)
@@ -78,7 +77,6 @@ int		evt_live_mouse_clicked(int x, int y, int z, t_mlx *m)
 {
 	(void)x;
 	g_mouse = (t_xy){.x = y, .y = z};
-	clear(*m);
 	if (SHOW_LIVE_MOUSE)
 	{
 		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
@@ -93,7 +91,6 @@ int		evt_live_mouse_pressed(int x, int y, int z, t_mlx *m)
 {
 	(void)x;
 	g_mouse = (t_xy){.x = y, .y = z};
-	mlx_clear_window(m->i, m->w);
 	if (SHOW_LIVE_MOUSE)
 	{
 		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
