885bb49bc96e35a7f15c0996b430b17d06b5ca74
Achraf <macadelic1030@gmail.com>
2019-02-06 11:12:44 +0100

    Ajout des couleurs etc;

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 08:58:10 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/02/06 09:42:15 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/02/06 10:23:43 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,34 +25,66 @@ t_key g_key[] = {
 	{ESC, &key_esc}
 };
 
-int		key_up(t_mlx m)
+int		evt_live_mouse_move(int x, int y, t_mlx *m)
 {
+	g_mouse = (t_xy){.x = x, .y = y};
+	clear(*m);
+	if (SHOW_LIVE_MOUSE)
+	{
+		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
+			ft_itoa_base(x, 10, 0));
+		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 50}, 0xFFFF00,
+			ft_itoa_base(y, 10, 0));
+	}
 	(void)m;
 	return (0);
 }
 
-int		key_down(t_mlx m)
+int		evt_live_key_clicked(int key, t_mlx *m)
 {
-	(void)m;
+	int i;
+
+	i = -1;
+	while (g_key[++i].key)
+		if (g_key[i].key == key)
+			g_key[i].function && g_key[i].function(*m);
+	clear(*m);
+	if (SHOW_LIVE_KEY)
+		string(*m, (t_xy){.x = g_mouse.x + 40, .y = g_mouse.y}, 0x00FF00,
+			ft_itoa_base(key, 10, 0));
+	(void)key;
 	return (0);
 }
 
-int		key_left(t_mlx m)
+int		evt_live_key_pressed(int key, t_mlx *m)
 {
-	(void)m;
+	int i;
+
+	i = -1;
+	while (g_key[++i].key)
+		if (g_key[i].key == key)
+			g_key[i].function && g_key[i].function(*m);
+	clear(*m);
+	if (SHOW_LIVE_KEY)
+	{
+		string(*m, (t_xy){.x = g_mouse.x + 40, .y = g_mouse.y}, 0x00FF00,
+			ft_itoa_base(key, 10, 0));
+	}
 	return (0);
 }
 
-int		key_right(t_mlx m)
+int		evt_live_mouse_clicked(int x, int y, int z, t_mlx *m)
 {
-	(void)m;
-	return (0);
-}
-
-int		key_esc(t_mlx m)
-{
-	(void)m;
-	exit(0);
+	(void)x;
+	g_mouse = (t_xy){.x = y, .y = z};
+	clear(*m);
+	if (SHOW_LIVE_MOUSE)
+	{
+		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
+			ft_itoa_base(y, 10, 0));
+		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 50}, 0xFFFF00,
+			ft_itoa_base(z, 10, 0));
+	}
 	return (0);
 }
 
@@ -67,52 +99,6 @@ int		evt_live_mouse_pressed(int x, int y, int z, t_mlx *m)
 			ft_itoa_base(y, 10, 0));
 		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 50}, 0xFFFF00,
 			ft_itoa_base(z, 10, 0));
-	}
-	return (0);
-}
-
-int		evt_live_mouse_move(int x, int y, t_mlx *m)
-{
-	g_mouse = (t_xy){.x = x, .y = y};
-	mlx_clear_window(m->i, m->w);
-	if (SHOW_LIVE_MOUSE)
-	{
-		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 30}, 0xFFFF00,
-			ft_itoa_base(x, 10, 0));
-		string(*m, (t_xy){.x = g_mouse.x, .y = g_mouse.y + 50}, 0xFFFF00,
-			ft_itoa_base(y, 10, 0));
-	}
-	(void)m;
-	return (0);
-}
-
-int		evt_live_key_pressed(int key, t_mlx *m)
-{
-	int i = -1;
-
-	while (g_key[++i].key)
-		if (g_key[i].key == key)
-			g_key[i].function && g_key[i].function(*m);
-	mlx_clear_window(m->i, m->w);
-	if (SHOW_LIVE_KEY)
-		string(*m, (t_xy){.x = g_mouse.x + 40, .y = g_mouse.y}, 0x00FF00,
-			ft_itoa_base(key, 10, 0));
-	(void)key;
-	return (0);
-}
-
-int		evt_live_key_move(int key, t_mlx *m)
-{
-	int i = -1;
-
-	while (g_key[++i].key)
-		if (g_key[i].key == key)
-			g_key[i].function && g_key[i].function(*m);
-	mlx_clear_window(m->i, m->w);
-	if (SHOW_LIVE_KEY)
-	{
-		string(*m, (t_xy){.x = g_mouse.x + 40, .y = g_mouse.y}, 0x00FF00,
-			ft_itoa_base(key, 10, 0));
 	}
 	return (0);
 }
--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/03 09:26:14 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/02/06 09:47:21 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/02/06 10:27:37 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,34 @@
 #include <stdio.h>
 #include "fdf.h"
 
+int clear(t_mlx m)
+{
+	mlx_clear_window(m.i, m.w);
+	return (menu(m));
+}
+
+int menu(t_mlx m)
+{
+	string(m, (t_xy){.x = 250, .y = 200}, 0xFFFF00, "Gros delire ca");
+	string(m, (t_xy){.x = 300, .y = 200}, 0xFF00FF, "mec je suis ou");
+	string(m, (t_xy){.x = 200, .y = 250}, 0x00FF00, "PTDR JFKOI LA");
+	string(m, (t_xy){.x = 200, .y = 300}, 0x2F2F2F, "JFK LA OSSI XD");
+	return (0);
+}
+
 int		fdf(void)
 {
 	t_mlx m;
 
 	m.i = mlx_init();
 	m.w = mlx_new_window(m.i, 2000, 2000, "GROS DELIRES");
-	string(m, (t_xy){.x = 250, .y = 200}, 0xFFFF00, "Gros delire ca");
-	string(m, (t_xy){.x = 300, .y = 200}, 0xFF00FF, "mec je suis ou");
-	string(m, (t_xy){.x = 200, .y = 250}, 0x00FF00, "PTDR JFKOI LA");
-	string(m, (t_xy){.x = 200, .y = 300}, 0x2F2F2F, "JFK LA OSSI XD");
-	mlx_hook(m.w, KMOVE, 0, evt_live_key_move, &m);
+	clear(m);
 	mlx_hook(m.w, KPRESS, 0, evt_live_key_pressed, &m);
+	mlx_hook(m.w, KCLICK, 0, evt_live_key_clicked, &m);
 	mlx_hook(m.w, MMOVE, 0, evt_live_mouse_move, &m);
+	mlx_hook(m.w, MCLICK, 0, evt_live_mouse_clicked, &m);
 	mlx_hook(m.w, MPRESS, 0, evt_live_mouse_pressed, &m);
 	mlx_loop(m.i);
+	printf("{%s}\n", mlx_col_name[0].color)
 	return (18);
 }
